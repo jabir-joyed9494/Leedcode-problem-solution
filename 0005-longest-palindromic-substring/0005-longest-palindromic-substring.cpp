b5521e6173a0deeb004d05dfb7f92de545c37327
jabir-joyed9494 <71986259+jabir-joyed9494@users.noreply.github.com>
2024-09-17 12:58:50 +0600

    Time: 65 ms (49.78%), Space: 174.1 MB (22.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-string func(string s,int left,int right)
-{
-    while(left>=0 && right<s.size() && s[left]==s[right])
+    string func(string s,int left,int right)
     {
-        left--;
-        right++;
+        while(left>=0 && right<s.size() && s[left]==s[right])
+        {
+            left--;
+            right++;
+        }
+        string ss=s.substr(left+1,right-left-1);
+        return ss;
     }
-  //  cout<<left+1<<" "<<right-left+1<<endl;
-    string ss="";
-    ss+=(s.substr(left+1,right-left-1));
-    return ss;
-
-}
     string longestPalindrome(string s) {
-        int n = s.size();
+      int n=s.size();
         if(n==1) return s;
-        string ans="",ss="";
-       for(int i=1;i<n;i++)
-       {
-           string s1=func(s,i,i);
-           string s2=func(s,i-1,i);
-          // cout<<"i=="<<i<<" "<<s1<<" "<<s2<<endl;
-           if(s1.size()>ans.size())
-           {
-            ans=s1;
-           }
-           if(s2.size()>ans.size())
-           {
-            ans=s2;
-           }
-       }
-       return ans;
+        string ans="";
+        for(int i=1;i<n;i++)
+        {
+            string s1=func(s,i,i);
+            string s2=func(s,i-1,i);
+            //cout<<s1<<" "<<s2<<endl;
+            if(s1.size()>ans.size())
+            {
+                ans=s1;
+            }
+            if(s2.size()>ans.size())
+            {
+                ans=s2;
+            }
+          //  cout<<"ans=="<<ans<<endl;
+        }
+        return ans;
     }
 };
