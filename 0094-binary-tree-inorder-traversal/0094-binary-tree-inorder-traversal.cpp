cbcf97a933da0508f73d97a5053c0340e4acf041
jabir-joyed9494 <71986259+jabir-joyed9494@users.noreply.github.com>
2024-09-18 22:25:51 +0600

    Time: 0 ms (100.00%), Space: 12.1 MB (5.01%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -13,12 +13,12 @@ class Solution {
 public:
     vector<int>v;
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root)
-        {
-            inorderTraversal(root->left);
-            v.push_back(root->val);
-            inorderTraversal(root->right);
-        }
+       if(root)
+       {
+           inorderTraversal(root->left);
+           v.push_back(root->val);
+           inorderTraversal(root->right);
+       }
         return v;
     }
 };
