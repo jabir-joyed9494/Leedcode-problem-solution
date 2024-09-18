22c81ab5fe4f565bb5d6e1b5b431d0ce628c3442
jabir-joyed9494 <71986259+jabir-joyed9494@users.noreply.github.com>
2024-09-18 19:59:21 +0600

    Time: 28 ms (27.76%), Space: 77.5 MB (76.35%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,28 +11,48 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-      ListNode* dummy=new ListNode();
-       ListNode* temp=dummy;
-        int carry=0;
-        while(l1!=nullptr || l2!=nullptr || carry==1)
+       ListNode* node=new ListNode(-1);
+        ListNode* dummy=node;
+        int sum=0,carry=0;
+        while(l1!=nullptr && l2!=nullptr)
         {
-            int sum=0;
-            if(l1!=nullptr)
-            {
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2!=nullptr)
-            {
-                sum+=l2->val;
-                l2=l2->next;
-            }
+            sum=(l1->val+l2->val);
             sum+=carry;
+           // cout<<"sum=="<<sum<<endl;
+            ListNode* temp=new ListNode(sum%10);
+           // cout<<"temp="<<temp->val<<endl;
+            dummy->next=temp;
+            dummy=dummy->next;
             carry=sum/10;
-            temp->next=new ListNode(sum%10);
-            temp=temp->next;
+            l1=l1->next;
+            l2=l2->next;
         }
-        return dummy->next;
-       
+        while(l1!=nullptr)
+        {
+            sum=carry+l1->val;
+            ListNode* temp=new ListNode(sum%10);
+             dummy->next=temp;
+            dummy=dummy->next;
+            carry=sum/10;
+            l1=l1->next;
+        }
+           while(l2!=nullptr)
+        {
+            sum=carry+l2->val;
+            ListNode* temp=new ListNode(sum%10);
+                dummy->next=temp;
+            dummy=dummy->next;
+            carry=sum/10;
+               l2=l2->next;
+        }
+       if(carry>0){ 
+           sum=carry;
+        ListNode* temp=new ListNode(sum%10);
+                dummy->next=temp;
+            dummy=dummy->next;
+       }
+        return node->next;
+        
+        
     }
 };
